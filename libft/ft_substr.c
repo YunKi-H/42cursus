e031976274fd80ff51a16d769ccce46fcd10d71a
DHDI-H <dbsrl759@naver.com>
2021-11-18 01:36:13 +0900

    11180136

--- libft/ft_substr.c
+++ libft/ft_substr.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_substr.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: yuhwang <yuhwang@student.42.fr>            +#+  +:+       +#+        */
+/*   By: maemi <maemi@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/16 20:38:27 by yuhwang           #+#    #+#             */
-/*   Updated: 2021/11/16 20:52:08 by yuhwang          ###   ########.fr       */
+/*   Updated: 2021/11/18 01:15:20 by maemi            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,15 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 	char	*ret;
 	size_t	i;
 
+	ret = (char *)malloc(sizeof(char) * (len + 1));
+	if (!ret)
+		return (NULL);
 	i = 0;
-	ret = (char *)malloc(len)
+	while (s[start + i] && i < len)
+	{
+		ret[i] = s[start + i];
+		i++;
+	}
+	ret[i] = 0;
+	return (ret);
 }
