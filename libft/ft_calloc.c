1ba08f0e372e7434fdf7f5cce61dbb47cbe82b4f
DHDI-H <dbsrl759@naver.com>
2021-11-22 17:41:08 +0900

    11221741

--- libft/ft_calloc.c
+++ libft/ft_calloc.c
@@ -6,7 +6,7 @@
 /*   By: maemi <maemi@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/08 13:06:49 by maemi             #+#    #+#             */
-/*   Updated: 2021/11/22 14:16:15 by maemi            ###   ########.fr       */
+/*   Updated: 2021/11/22 17:40:38 by maemi            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,9 +17,9 @@ void	*ft_calloc(size_t count, size_t size)
 	void	*result;
 	size_t	tsize;
 
-	tsize = count * size;
 	if (count && size > (size_t)-1 / count)
 		return (NULL);
+	tsize = count * size;
 	result = malloc(tsize);
 	if (!result)
 		return (NULL);
--- libft/ft_memset.c
+++ libft/ft_memset.c
@@ -6,7 +6,7 @@
 /*   By: maemi <maemi@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/08 13:07:22 by maemi             #+#    #+#             */
-/*   Updated: 2021/11/10 16:11:15 by maemi            ###   ########.fr       */
+/*   Updated: 2021/11/22 17:40:44 by maemi            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,10 +14,12 @@
 
 void	*ft_memset(void *b, int c, size_t len)
 {
-	size_t	i;
+	size_t			i;
+	unsigned char	*tmp;
 
+	tmp = (unsigned char *)b;
 	i = 0;
 	while (i < len)
-		*(unsigned char *)(b + i++) = (unsigned char)c;
+		*(tmp + i++) = (unsigned char)c;
 	return (b);
 }
