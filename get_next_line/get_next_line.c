8115d95ba67aa42f35c8980ae9a5b5834a6ffcac
maemi <maemi@maemiuicBookPro.com>
2021-12-14 14:19:17 +0900

    gnl

--- get_next_line/get_next_line.c
+++ get_next_line/get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: yuhwang <yuhwang@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/28 20:21:23 by yuhwang           #+#    #+#             */
-/*   Updated: 2021/12/09 20:00:39 by yuhwang          ###   ########.fr       */
+/*   Updated: 2021/12/11 15:36:05 by yuhwang          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,29 +17,37 @@ char	*get_next_line(int fd)
 	static char	*note[OPEN_MAX];
 	char		*buf;
 	char		*tmp;
-	int			len;
+	ssize_t		len;
+	ssize_t		index;
 
 	if (fd < 0 || BUFFER_SIZE < 1)
 		return (NULL);
-	note[fd] = ft_strdup("");
-	if (!note[fd])
-		return (NULL);
+	if (!note[fd] || !note[fd][0])
+		note[fd] = ft_strdup("");
 	buf = (char *)malloc(sizeof(char) * (BUFFER_SIZE + 1));
 	if (!buf)
 		return (NULL);
-	while (!ft_strchr(note[fd], '\n'))
+	while (!ft_strchr(note[fd], '\n') && !(len < BUFFER_SIZE))
 	{
 		len = read(fd, buf, BUFFER_SIZE);
+		if (!len)
+			return (NULL);
 		buf[len] = '\0';
 		tmp = ft_strjoin(note[fd], buf);
 		free(note[fd]);
 		note[fd] = tmp;
-		if (len < BUFFER_SIZE)
-			break ;
 	}
-	tmp = ft_substr(note[fd], 0, ft_strchr(note[fd], '\n') - note[fd]);
+	if (ft_strchr(note[fd], '\n'))
+	{
+		index = ft_strchr(note[fd], '\n') - note[fd];
+		tmp = ft_substr(note[fd], 0, index);
+		note[fd] += index + 1;
+	}
+	else
+	{
+		tmp = ft_strdup(note[fd]);
+		free(note[fd]);
+	}
 	free(buf);
-	if (len < BUFFER_SIZE)
-			free(note[fd]);
 	return (tmp);
 }
