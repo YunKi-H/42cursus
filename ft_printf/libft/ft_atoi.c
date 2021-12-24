f74324d115435f7e4fd9dd2b882a149f1bd17499
maemi <maemi@maemiuicBookPro.com>
2021-12-24 14:29:01 +0900

    pf

---
+++ ft_printf/libft/ft_atoi.c
@@ -0,0 +1,51 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_atoi.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yuhwang <yuhwang@student.42seoul.kr>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/11/08 13:06:42 by yuhwang           #+#    #+#             */
+/*   Updated: 2021/11/26 12:40:49 by yuhwang          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+static int	ft_isspace(int c)
+{
+	if (c == '\t')
+		return (1);
+	if (c == '\n')
+		return (1);
+	if (c == '\v')
+		return (1);
+	if (c == '\f')
+		return (1);
+	if (c == '\r')
+		return (1);
+	if (c == ' ')
+		return (1);
+	return (0);
+}
+
+int	ft_atoi(const char *str)
+{
+	int	result;
+	int	positive;
+
+	positive = 1;
+	result = 0;
+	while (ft_isspace(*str))
+		str++;
+	if (*str == '-' || *str == '+')
+	{
+		if (*str == '-')
+			positive = -positive;
+		str++;
+	}
+	while (*str >= '0' && *str <= '9')
+	{
+		result = result * 10 + *str - '0';
+		str++;
+	}
+	return (positive * result);
+}
