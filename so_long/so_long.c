ba4c3abd81879bc5d1b85b69d3576231eb5219a1
maemi <dbsrl759@naver.com>
2022-04-24 04:55:05 +0900

    sl

--- so_long/so_long.c
+++ so_long/so_long.c
@@ -6,22 +6,40 @@
 /*   By: yuhwang <yuhwang@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/21 10:53:20 by yuhwang           #+#    #+#             */
-/*   Updated: 2022/04/21 14:59:43 by yuhwang          ###   ########.fr       */
+/*   Updated: 2022/04/24 04:40:00 by yuhwang          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "so_long.h"
 #include "minilibx_mms/mlx.h"
 
+void	my_mlx_pixel_put(t_data *data, int x, int y, int color)
+{
+	char	*dst;
+
+	dst = data->addr + (y * data->line_length + x * (data->bits_per_pixel / 8));
+	*(unsigned int*)dst = color;
+}
+
 int	main(int argc, char **argv)
 {
 	void	*mlx_ptr;
 	void	*win_ptr;
+	t_data	image;
 
 	if (!argv || !argc)
 		return (0);
 	mlx_ptr = mlx_init();
 	win_ptr = mlx_new_window(mlx_ptr, 500, 500, "mlx 42");
+	image.img = mlx_new_image(mlx_ptr, 500, 500);
+	image.addr = mlx_get_data_addr(image.img, &image.bits_per_pixel, &image.line_length, &image.endian);
+	for (int i = 0; i < 400; i++)
+	{
+		for (int j = 0; j < 400; j++)
+			mlx_pixel_put(mlx_ptr, win_ptr, i, j, 0x00FFFFFF);
+	}
+	my_mlx_pixel_put(&image, 200, 300, 0x00FFFFFF);
+	mlx_put_image_to_window(mlx_ptr, win_ptr, image.img, 0, 0);
 	mlx_loop(mlx_ptr);
 	if (!win_ptr)
 		return (0);
--- so_long/so_long.h
+++ so_long/so_long.h
@@ -6,13 +6,27 @@
 /*   By: yuhwang <yuhwang@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/21 10:53:24 by yuhwang           #+#    #+#             */
-/*   Updated: 2022/04/21 14:59:45 by yuhwang          ###   ########.fr       */
+/*   Updated: 2022/04/24 04:42:05 by yuhwang          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef SO_LONG_H
 # define SO_LONG_H
+# define KEY_ESC 53
+# define KEY_W 13
+# define KEY_A 0
+# define KEY_S 1
+# define KEY_D 2
 
+# include <unistd.h>
 
+typedef struct s_data
+{
+	void 	*img;
+	char	*addr;
+	int		bits_per_pixel;
+	int		line_length;
+	int		endian;
+}		t_data;
 
 #endif
