d9fc130a3f7cd85d18fdbe4792d4076931690223
maemi <dbsrl759@naver.com>
2022-04-25 07:51:51 +0900

    sl

--- so_long/so_long.c
+++ so_long/so_long.c
@@ -6,11 +6,14 @@
 /*   By: yuhwang <yuhwang@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/21 10:53:20 by yuhwang           #+#    #+#             */
-/*   Updated: 2022/04/24 04:40:00 by yuhwang          ###   ########.fr       */
+/*   Updated: 2022/04/24 17:48:18 by yuhwang          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <stdlib.h>
+#include <unistd.h>
 #include "so_long.h"
+#include "../libft/libft.h"
 #include "minilibx_mms/mlx.h"
 
 void	my_mlx_pixel_put(t_data *data, int x, int y, int color)
@@ -21,27 +24,62 @@ void	my_mlx_pixel_put(t_data *data, int x, int y, int color)
 	*(unsigned int*)dst = color;
 }
 
+int	key_hook(int keycode, t_vars *vars)
+{
+	if (keycode == KEY_ESC)
+	{
+		mlx_destroy_window(vars->mlx, vars->win);
+		exit(0);
+	}
+	return (0);
+}
+
+int	exit_hook()
+{
+	exit(0);
+}
+
+int	create_trgb(int t, int r, int g, int b)
+{
+	return (t << 24 | r << 16 | g << 8 | b);
+}
+
 int	main(int argc, char **argv)
 {
-	void	*mlx_ptr;
-	void	*win_ptr;
+	int	color;
 	t_data	image;
+	t_vars	vars;
 
-	if (!argv || !argc)
-		return (0);
-	mlx_ptr = mlx_init();
-	win_ptr = mlx_new_window(mlx_ptr, 500, 500, "mlx 42");
-	image.img = mlx_new_image(mlx_ptr, 500, 500);
+	if (!argc || !argv)
+		;
+	int	img_width = 1920;
+	int	img_height = 1080;
+
+	vars.mlx = mlx_init();
+	vars.win = mlx_new_window(vars.mlx, img_width, img_height, "mlx 42");
+	image.img = mlx_new_image(vars.mlx, img_width, img_height);
 	image.addr = mlx_get_data_addr(image.img, &image.bits_per_pixel, &image.line_length, &image.endian);
-	for (int i = 0; i < 400; i++)
+	for (int i = 0; i < img_height - 1; i++)
 	{
-		for (int j = 0; j < 400; j++)
-			mlx_pixel_put(mlx_ptr, win_ptr, i, j, 0x00FFFFFF);
+		for (int j = 0; j < img_width - 1; j++)
+		{
+			double r = (double)(img_width - j) / (img_height - 1);
+			double g = (double)(i) / (img_height - 1);
+			double b = 1;
+			color = create_trgb(0, 255.999 * r, 255.999 * g, 255.999 * b);
+			my_mlx_pixel_put(&image, j, i, color);
+		}
 	}
-	my_mlx_pixel_put(&image, 200, 300, 0x00FFFFFF);
-	mlx_put_image_to_window(mlx_ptr, win_ptr, image.img, 0, 0);
-	mlx_loop(mlx_ptr);
-	if (!win_ptr)
-		return (0);
+	mlx_put_image_to_window(vars.mlx, vars.win, image.img, 0, 0);
+	mlx_key_hook(vars.win, key_hook, &vars);
+	mlx_hook(vars.win, 17, 0, exit_hook, 0);
+	mlx_loop(vars.mlx);
 	return (0);
+}
+
+void	print_error(const char *msg)
+{
+	write(2, "Error\n",6);
+	write(2, msg, ft_strlen(msg));
+	exit(1);
 }
--- so_long/so_long.h
+++ so_long/so_long.h
@@ -6,7 +6,7 @@
 /*   By: yuhwang <yuhwang@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/21 10:53:24 by yuhwang           #+#    #+#             */
-/*   Updated: 2022/04/24 04:42:05 by yuhwang          ###   ########.fr       */
+/*   Updated: 2022/04/24 17:56:33 by yuhwang          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,8 +17,37 @@
 # define KEY_A 0
 # define KEY_S 1
 # define KEY_D 2
+# define IS_NOT_SEALED "MAP IS NOT SEALED"
+# define IS_NOT_SQUARE "MAP IS NOT SQUARE"
+# define NO_EXIT "MAP HAS NO EXIT"
+# define NO_COLLECTIBLES "MAP HAS NO COLLETIBLES"
+# define NO_START_POINT "MAP HAS NO START POINT"
+# define MALLOC_FAILED "MEMORY ALLOCATION FAILED"
 
-# include <unistd.h>
+# include <stddef.h>
+# include "get_next_line.h"
+
+typedef struct s_coor
+{
+	int	x;
+	int	y;
+}	t_coor;
+
+typedef struct s_map
+{
+	char	**map;
+	size_t	width;
+	size_t	height;
+	size_t	collectibles;
+	t_coor	exit;
+	t_coor	player;
+}	t_map;
+
+typedef struct s_vars
+{
+	void	*mlx;
+	void	*win;
+}	t_vars;
 
 typedef struct s_data
 {
@@ -27,6 +56,6 @@ typedef struct s_data
 	int		bits_per_pixel;
 	int		line_length;
 	int		endian;
-}		t_data;
+}	t_data;
 
 #endif
