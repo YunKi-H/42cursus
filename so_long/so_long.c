ef0ad8d481011bdf9328fe9fe6bd964983b909c7
maemi <dbsrl759@naver.com>
2022-04-29 21:38:33 +0900

    sl_parse_map

--- so_long/so_long.c
+++ so_long/so_long.c
@@ -6,7 +6,7 @@
 /*   By: yuhwang <yuhwang@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/21 10:53:20 by yuhwang           #+#    #+#             */
-/*   Updated: 2022/04/27 16:40:53 by yuhwang          ###   ########.fr       */
+/*   Updated: 2022/04/29 21:37:20 by yuhwang          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,112 +14,175 @@
 #include <unistd.h>
 #include <stdio.h>
 #include <fcntl.h>
+#include <limits.h>
 #include "so_long.h"
 #include "../libft/libft.h"
 #include "minilibx_mms/mlx.h"
 
-void	my_mlx_pixel_put(t_data *data, int x, int y, int color)
-{
-	char	*dst;
-
-	dst = data->addr + (y * data->line_length + x * (data->bits_per_pixel / 8));
-	*(unsigned int*)dst = color;
-}
-
-int	key_hook(int keycode, t_vars *vars)
-{
-	if (keycode == KEY_ESC)
-	{
-		mlx_destroy_window(vars->mlx, vars->win);
-		exit(0);
-	}
-	return (0);
-}
-
-int	exit_hook()
-{
-	exit(0);
-}
-
-int	create_trgb(int t, int r, int g, int b)
-{
-	return (t << 24 | r << 16 | g << 8 | b);
-}
-
 int	main(int argc, char **argv)
 {
-	// int	color;
-	// t_data	image;
-	// t_vars	vars;
+	t_map	*map;
 
 	if (argc < 2)
 	{
 		write(1, "USAGE : \"./so_long [MAP.ber]\"\n", 31);
 		exit(0);
 	}
-	int		fd;
-	// t_map	map;
-	printf("%s\n", argv[1]);
-	fd = open(argv[1], O_RDONLY);
-	printf("fd = %d\n", fd);
-	// char temp[10];
-	// temp[9] = 0;
-	// read(fd, temp, 9);
-	// printf("%s\n", temp);
-	// write(3, "test\n", 5);
-	while (1)
-	{
-		char	*buf;
-		size_t	len;
+	map = map_init();
+	map_get_info(argv[1], map);
+	map_read_file(argv[1], map);
+	//print map
+	size_t	i = 0;
+	while (i < map->height)
+		printf("%s", map->map[i++]);
 
-		buf = get_next_line(fd);
-		printf("%s", buf);
-		if (!buf)
-			break ;
-		len = ft_strlen(buf);
-		len++;
-	}
-
-	// int	img_width = 1080;
-	// int	img_height = 720;
-
-	// vars.mlx = mlx_init();
-	// vars.win = mlx_new_window(vars.mlx, img_width, img_height, "mlx 42");
-	// // image.img = mlx_new_image(vars.mlx, img_width, img_height);
-	// image.img = mlx_xpm_file_to_image(vars.mlx, "./resource/pacman_down.xpm", &img_width, &img_height);
-	// image.addr = mlx_get_data_addr(image.img, &image.bits_per_pixel, &image.line_length, &image.endian);
-	// // for (int i = 0; i < img_height - 1; i++)
-	// // {
-	// // 	for (int j = 0; j < img_width - 1; j++)
-	// // 	{
-	// // 		double r = (double)(img_width - j) / (img_height - 1);
-	// // 		double g = (double)(i) / (img_height - 1);
-	// // 		double b = 1;
-	// // 		color = create_trgb(0, 255.999 * r, 255.999 * g, 255.999 * b);
-	// // 		my_mlx_pixel_put(&image, j, i, color);
-	// // 	}
-	// // }
-	// for (int i = 0; i < img_height - 1; i += 50)
-	// {
-	// 	for (int j = 0; j < img_width - 1; j += 50)
-	// 	{
-	// 		printf("i = %d, j = %d\n", i, j);
-	// 		mlx_put_image_to_window(vars.mlx, vars.win, image.img, j, i);
-	// 	}
-	// }
-	// // mlx_put_image_to_window(vars.mlx, vars.win, image.img, 0, 0);
-	// // mlx_put_image_to_window(vars.mlx, vars.win, image.img, 50, 0);
-	// // mlx_put_image_to_window(vars.mlx, vars.win, image.img, 0, 50);
-	// // mlx_put_image_to_window(vars.mlx, vars.win, image.img, 50, 50);
-	// mlx_key_hook(vars.win, key_hook, &vars);
-	// mlx_hook(vars.win, 17, 0, exit_hook, 0);
-	// mlx_loop(vars.mlx);
 	return (0);
 }
 
-void	print_error(const char *msg)
+t_map	*map_init(void)
+{
+	t_map	*map;
+
+	map = (t_map *)malloc(sizeof(t_map));
+	map->map = NULL;
+	map->width = 0;
+	map->height = 0;
+	map->coin_count = 0;
+	map->player_count = 0;
+	map->exit_count = 0;
+	return (map);
+}
+
+void	map_get_info(const char *file, t_map *map)
+{
+	int	fd;
+
+	isvalid_extension(file);
+	fd = open(file, O_RDONLY);
+	if (fd < 0 || fd > OPEN_MAX)
+		print_error(2, INVALID_FD);
+	while (read_line_f(fd, map))
+		;
+	if (!map->coin_count)
+		print_error(5, NO_COLLECTIBLES);
+	if (map->player_count != 1)
+		print_error(5, NO_START_POINT);
+	if (map->exit_count != 1)
+		print_error(5, NO_EXIT);
+	close(fd);
+}
+
+void	isvalid_extension(const char *file)
+{
+	char	*tmp;
+
+	tmp = ft_strrchr(file, '.');
+	if (!tmp)
+		print_error(4, INVALID_FILE);
+	if (ft_strncmp(tmp, ".ber", 4))
+		print_error(4, INVALID_FILE);
+	if (*(tmp + 4))
+		print_error(4, INVALID_FILE);
+}
+
+int	read_line_f(int fd, t_map *map)
+{
+	char	*buf;
+	size_t	len;
+	int		i;
+
+	buf = get_next_line(fd);
+	if (!buf)
+		return (0);
+	len = ft_strlen(buf);
+	if (map->width && map->width != len)
+		print_error(4, IS_NOT_SQUARE);
+	map->width = len;
+	map->height += 1;
+	i = 0;
+	while (buf[i] != '\n' && buf[i])
+	{
+		isvalid_char(buf[i], map);
+		i += 1;
+	}
+	free(buf);
+	return (1);
+}
+
+void	isvalid_char(char c, t_map *map)
+{
+	if (c == 'C')
+		map->coin_count += 1;
+	else if (c == 'P')
+		map->player_count += 1;
+	else if (c == 'E')
+		map->exit_count += 1;
+	else if (c != '0' && c != '1')
+		print_error(3, "MAP HAS WRONG CHARACTER");
+}
+
+void	map_read_file(const char *file, t_map *map)
+{
+	int		fd;
+	size_t	i;
+
+	map->map = (char **)malloc(sizeof(char *) * map->height);
+	if (!map->map)
+		print_error(1, MALLOC_FAILED);
+	fd = open(file, O_RDONLY);
+	if (fd < 0 || fd > OPEN_MAX)
+		print_error(2, INVALID_FD);
+	i = 0;
+	while (i < map->height)
+	{
+		map->map[i] = get_next_line(fd);
+		i += 1;
+	}
+	if (!isvalid_sealed(map, map->width, map->height))
+		print_error(4, IS_NOT_SEALED);
+}
+
+int	isvalid_sealed(t_map *map, size_t width, size_t height)
+{
+	size_t	i;
+
+	i = 0;
+	if (!isvalid_edge(map->map[i], width))
+		return (0);
+	i += 1;
+	while (i < height - 1)
+	{
+		if (!isvalid_mid(map->map[i], width))
+			return (0);
+		i += 1;
+	}
+	if (!isvalid_edge(map->map[i], width))
+		return (0);
+	return (1);
+}
+
+int	isvalid_edge(char *line, size_t length)
+{
+	size_t	i;
+
+	i = 0;
+	while (i < length - 1)
+	{
+		if (line[i] != '1')
+			return (0);
+		i += 1;
+	}
+	return (1);
+}
+
+int	isvalid_mid(char *line, size_t length)
+{
+	return (line[0] == '1' && line[length - 2] == '1');
+}
+
+void	print_error(int errnum, const char *msg)
 {
 	write(2, "Error\n",6);
 	write(2, msg, ft_strlen(msg));
-	exit(1);
+	exit(errnum);
 }
--- so_long/so_long.h
+++ so_long/so_long.h
@@ -6,7 +6,7 @@
 /*   By: yuhwang <yuhwang@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/21 10:53:24 by yuhwang           #+#    #+#             */
-/*   Updated: 2022/04/24 17:56:33 by yuhwang          ###   ########.fr       */
+/*   Updated: 2022/04/29 21:13:04 by yuhwang          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,12 +17,14 @@
 # define KEY_A 0
 # define KEY_S 1
 # define KEY_D 2
-# define IS_NOT_SEALED "MAP IS NOT SEALED"
-# define IS_NOT_SQUARE "MAP IS NOT SQUARE"
-# define NO_EXIT "MAP HAS NO EXIT"
-# define NO_COLLECTIBLES "MAP HAS NO COLLETIBLES"
-# define NO_START_POINT "MAP HAS NO START POINT"
-# define MALLOC_FAILED "MEMORY ALLOCATION FAILED"
+# define INVALID_FILE "HAVE TO PUT [.ber] FILE\n"
+# define INVALID_FD "FAIL TO OPEN FILE\n"
+# define IS_NOT_SEALED "MAP IS NOT SEALED\n"
+# define IS_NOT_SQUARE "MAP IS NOT SQUARE\n"
+# define NO_EXIT "MAP HAS NO EXIT\n"
+# define NO_COLLECTIBLES "MAP HAS NO COLLETIBLES\n"
+# define NO_START_POINT "MAP HAS NO START POINT\n"
+# define MALLOC_FAILED "MEMORY ALLOCATION FAILED\n"
 
 # include <stddef.h>
 # include "get_next_line.h"
@@ -38,24 +40,20 @@ typedef struct s_map
 	char	**map;
 	size_t	width;
 	size_t	height;
-	size_t	collectibles;
-	t_coor	exit;
-	t_coor	player;
+	size_t	coin_count;
+	size_t	player_count;
+	size_t	exit_count;
 }	t_map;
 
-typedef struct s_vars
-{
-	void	*mlx;
-	void	*win;
-}	t_vars;
-
-typedef struct s_data
-{
-	void 	*img;
-	char	*addr;
-	int		bits_per_pixel;
-	int		line_length;
-	int		endian;
-}	t_data;
+t_map	*map_init(void);
+void	map_get_info(const char *file, t_map *map);
+void	isvalid_extension(const char *file);
+int		read_line_f(int fd, t_map *map);
+void	isvalid_char(char c, t_map *map);
+void	map_read_file(const char *file, t_map *map);
+int		isvalid_sealed(t_map *map, size_t width, size_t height);
+int		isvalid_edge(char *line, size_t length);
+int		isvalid_mid(char *line, size_t length);
+void	print_error(int errnum, const char *msg);
 
 #endif
