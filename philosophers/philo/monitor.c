624ddf5f7848597d1458193896aff955bd6bf097
maemi <dbsrl759@naver.com>
2022-06-27 18:22:28 +0900

    [fix] norm

--- philosophers/philo/monitor.c
+++ philosophers/philo/monitor.c
@@ -6,7 +6,7 @@
 /*   By: yuhwang <yuhwang@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/27 15:37:09 by yuhwang           #+#    #+#             */
-/*   Updated: 2022/06/27 18:06:15 by yuhwang          ###   ########.fr       */
+/*   Updated: 2022/06/27 18:21:39 by yuhwang          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,6 +24,20 @@ static void	print_dying_msg(t_philo *philo, const char *msg)
 	pthread_mutex_unlock(&philo->args->print);
 }
 
+static int	raise_hand_if_dead(t_args *arg, int i)
+{
+	pthread_mutex_lock(&arg->philos[i]->meal);
+	if (get_time() > arg->philos[i]->last_meal + arg->time_to_die)
+	{
+		set_variable(&arg->someone_dead, &arg->game_over, 1);
+		print_dying_msg(arg->philos[i], "died");
+		pthread_mutex_unlock(&arg->philos[i]->meal);
+		return (1);
+	}
+	pthread_mutex_unlock(&arg->philos[i]->meal);
+	return (0);
+}
+
 void	*monitoting(void *args)
 {
 	t_args *const	arg = (t_args *)args;
@@ -36,14 +50,8 @@ void	*monitoting(void *args)
 		i = -1;
 		while (++i < arg->number_of_philosophers)
 		{
-			pthread_mutex_lock(&arg->philos[i]->meal);
-			if (get_time() > arg->philos[i]->last_meal + arg->time_to_die)
-			{
-				set_variable(&arg->someone_dead, &arg->game_over, 1);
-				print_dying_msg(arg->philos[i], "died");
+			if (raise_hand_if_dead(arg, i))
 				return ((void *)arg->philos[i]);
-			}
-			pthread_mutex_unlock(&arg->philos[i]->meal);
 			pthread_mutex_lock(&arg->philos[i]->count);
 			if (arg->philos[i]->eat_count < arg->times_to_eat)
 				true_ending = 0;
