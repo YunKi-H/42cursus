7e8ee08682fedbfb9299eab5e49eca087d77c1c8
maemi <dbsrl759@naver.com>
2022-08-12 18:10:46 +0900

    [done] cpp01 ex00

--- cpp_module/cpp_module01/ex00/main.cpp
+++ cpp_module/cpp_module01/ex00/main.cpp
@@ -2,20 +2,20 @@
 #include "Zombie.hpp"
 
 int main() {
-	std::cout << "-----Default Zombie-----" << std::endl;
+	std::cout << "-----Default Zombie-----\n" << std::endl;
 	Zombie z1 = Zombie();
-	std::cout << std::endl;
-	std::cout << "------Named Zombie------" << std::endl;
+
+	std::cout << "------Named Zombie------\n" << std::endl;
 	Zombie z2 = Zombie("Named Zombie");
-	std::cout << std::endl;
-	std::cout << "------Heap Zombie-------" << std::endl;
+
+	std::cout << "------Heap Zombie-------\n" << std::endl;
 	Zombie* z3 = Zombie::newZombie("Heap Zombie");
-	std::cout << std::endl;
-	std::cout << "------Stack Zombie------" << std::endl;
+
+	std::cout << "------Stack Zombie------\n" << std::endl;
 	Zombie::randomChump("Stack Zombie");
-	std::cout << std::endl;
-	std::cout << "---Delete Heap Zombie---" << std::endl;
+
+	std::cout << "---Delete Heap Zombie---\n" << std::endl;
 	delete z3;
-	std::cout << "------------------------" << std::endl;
-	std::cout << std::endl;
+	std::cout << "------------------------\n" << std::endl;
+
 }
