8f55f51523db8ab61a23121f98bc0e8cb982b432
maemi <dbsrl759@naver.com>
2022-09-09 17:57:45 +0900

    [done] cpp05 ex00

--- cpp_module/cpp_module05/ex00/Bureaucrat.cpp
+++ cpp_module/cpp_module05/ex00/Bureaucrat.cpp
@@ -1,7 +1,65 @@
 #include "Bureaucrat.hpp"
 
-Bureaucrat::Bureaucrat() {
+Bureaucrat::Bureaucrat() : _name("a Bureaucrat"), _grade(Bureaucrat::_gradeLowest) {
+}
+
+Bureaucrat::Bureaucrat(const std::string &name) : _name(name), _grade(Bureaucrat::_gradeLowest) {
+}
+
+Bureaucrat::Bureaucrat(const Bureaucrat &bureaucrat) {
+	*this = bureaucrat;
 }
 
 Bureaucrat::~Bureaucrat() {
+}
+
+Bureaucrat &Bureaucrat::operator=(const Bureaucrat &rhs) {
+	const_cast<std::string &>(this->_name) = rhs.getName();
+	this->_grade = rhs.getGrade();
+	return *this;
+}
+
+const std::string &Bureaucrat::getName() const {
+	return this->_name;
+}
+
+const int &Bureaucrat::getGrade() const {
+	return this->_grade;
+}
+
+void Bureaucrat::increGrade(const int &amount) {
+	try {
+		this->_grade -= amount;
+		if (this->_grade < 1) {
+			this->_grade = 1;
+			throw Bureaucrat::GradeTooHighException();
+		}
+	} catch(const std::exception& e) {
+		std::cerr << e.what() << std::endl;
+	}
+}
+
+void Bureaucrat::decreGrade(const int &amount) {
+	try {
+		this->_grade += amount;
+		if (this->_grade > 150) {
+			this->_grade = 150;
+			throw Bureaucrat::GradeTooLowException();
+		}
+	} catch(const std::exception& e) {
+		std::cerr << e.what() << std::endl;
+	}
+}
+
+const char *Bureaucrat::GradeTooHighException::what() const throw() {
+	return "Grade Too High!";
+}
+
+const char *Bureaucrat::GradeTooLowException::what() const throw() {
+	return "Grade Too Low!";
+}
+
+std::ostream &operator<<(std::ostream &out, const Bureaucrat &bureaucrat) {
+	return out << bureaucrat.getName()
+	<< ", bureaucrat grade " << bureaucrat.getGrade();
 }
