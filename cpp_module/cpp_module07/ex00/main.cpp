030a1a778b8b3e94405fe63612a8d0bc5137393a
maemi <dbsrl759@naver.com>
2022-09-16 23:33:16 +0900

    [add] subject's test case

--- cpp_module/cpp_module07/ex00/main.cpp
+++ cpp_module/cpp_module07/ex00/main.cpp
@@ -4,18 +4,18 @@
 #include <string>
 
 int main() {
-	int a = 10, b = 42;
+	int i = 10, j = 42;
 	std::string hello("hello");
 	std::string hi("hi");
 
-	std::cout << "a: " << a << std::endl;
-	std::cout << "b: " << b << std::endl;
+	std::cout << "i: " << i << std::endl;
+	std::cout << "j: " << j << std::endl;
 	std::cout << "After swap" << std::endl;
-	swap(a, b);
-	std::cout << "a: " << a << std::endl;
-	std::cout << "b: " << b << std::endl;
-	std::cout << "min: " << min(a, b) << std::endl;
-	std::cout << "max: " << max(a, b) << std::endl;
+	swap(i, j);
+	std::cout << "a: " << i << std::endl;
+	std::cout << "b: " << j << std::endl;
+	std::cout << "min: " << min(i, j) << std::endl;
+	std::cout << "max: " << max(i, j) << std::endl;
 	std::cout << std::endl;
 	std::cout << "hello: " << hello << std::endl;
 	std::cout << "hi   : " << hi << std::endl;
@@ -25,4 +25,19 @@ int main() {
 	std::cout << "hi   : " << hi << std::endl;
 	std::cout << "min: " << min(hello, hi) << std::endl;
 	std::cout << "max: " << max(hello, hi) << std::endl;
+	std::cout << std::endl;
+
+	int a = 2;
+	int b = 3;
+	::swap( a, b );
+	std::cout << "a = " << a << ", b = " << b << std::endl;
+	std::cout << "min( a, b ) = " << ::min( a, b ) << std::endl;
+	std::cout << "max( a, b ) = " << ::max( a, b ) << std::endl;
+	std::string c = "chaine1";
+	std::string d = "chaine2";
+	::swap(c, d);
+	std::cout << "c = " << c << ", d = " << d << std::endl;
+	std::cout << "min( c, d ) = " << ::min( c, d ) << std::endl;
+	std::cout << "max( c, d ) = " << ::max( c, d ) << std::endl;
+	return 0;
 }
--- cpp_module/cpp_module07/ex00/whatever.hpp
+++ cpp_module/cpp_module07/ex00/whatever.hpp
@@ -10,7 +10,7 @@ void swap(T &a, T &b) {
 
 template <typename T>
 T &min(T &a, T &b) {
-	return a <= b ? a : b;
+	return a < b ? a : b;
 }
 
 template <typename T>
