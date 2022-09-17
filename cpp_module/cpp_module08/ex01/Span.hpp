83df5c443eb7be8b4efe80327ab270aa121717dc
maemi <dbsrl759@naver.com>
2022-09-18 08:16:28 +0900

    [init] cpp08 ex01

---
+++ cpp_module/cpp_module08/ex01/Span.hpp
@@ -0,0 +1,18 @@
+#ifndef SPAN_HPP
+# define SPAN_HPP
+
+class Span {
+private:
+	/* data */
+public:
+	Span(/* args */);
+	~Span();
+};
+
+Span::Span(/* args */) {
+}
+
+Span::~Span() {
+}
+
+#endif
