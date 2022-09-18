d3f90ca516e83e50bd53b9511bcdc96719d1e501
maemi <dbsrl759@naver.com>
2022-09-18 13:18:51 +0900

    [done] cpp08 ex02

--- cpp_module/cpp_module08/ex02/main.cpp
+++ cpp_module/cpp_module08/ex02/main.cpp
@@ -1,7 +1,10 @@
 #include "MutantStack.hpp"
 #include <iostream>
+#include <vector>
+#include <list>
 
 int main() {
+	std::cout << "--MutantStack--" << std::endl;
 	MutantStack<int> mstack;
 	mstack.push(5);
 	mstack.push(17);
@@ -22,5 +25,47 @@ int main() {
 		++it;
 	}
 	std::stack<int> s(mstack);
+
+	std::cout << "\n--list--" << std::endl;
+	std::list<int> li;
+	li.push_back(5);
+	li.push_back(17);
+	std::cout << li.back() << std::endl;
+	li.pop_back();
+	std::cout << li.size() << std::endl;
+	li.push_back(3);
+	li.push_back(5);
+	li.push_back(737);
+	//[...]
+	li.push_back(0);
+	std::list<int>::iterator lit = li.begin();
+	std::list<int>::iterator lite = li.end();
+	++lit;
+	--lit;
+	while (lit != lite) {
+		std::cout << *lit << std::endl;
+		++lit;
+	}
+
+	std::cout << "\n--vector--" << std::endl;
+	std::vector<int> vec;
+	vec.push_back(5);
+	vec.push_back(17);
+	std::cout << vec.back() << std::endl;
+	vec.pop_back();
+	std::cout << vec.size() << std::endl;
+	vec.push_back(3);
+	vec.push_back(5);
+	vec.push_back(737);
+	//[...]
+	vec.push_back(0);
+	std::vector<int>::iterator vit = vec.begin();
+	std::vector<int>::iterator vite = vec.end();
+	++vit;
+	--vit;
+	while (vit != vite) {
+		std::cout << *vit << std::endl;
+		++vit;
+	}
 	return 0;
 }
